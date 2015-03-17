e3d3ae3a1144e049f5901cda38b4ac3136b69760
pierrecnalb <pierrecnalb@mailbox.org>
2015-03-17 23:41:46 +0100

    update git conflict

--- MainWindow.py
+++ MainWindow.py
@@ -21,31 +21,26 @@ def main():
     app.exec_()
 
 class MainWidget(QWidget):
-    #QMainWindow does not allow any self.main_grid or boxes layout. Therefore we use a QWidget instance
+    #QMainWindow does not allow any self.main_layout or boxes layout. Therefore we use a QWidget instance
     def __init__(self):
         QWidget.__init__(self)
         self.all_action_descriptors = []
         character_replacement_inputs = []
         character_replacement_inputs.append(Renamer.ActionInput('old_char', 'replace', str))
         character_replacement_inputs.append(Renamer.ActionInput('new_char', 'with', str))
-
         character_insertion_inputs = []
         character_insertion_inputs.append(Renamer.ActionInput('new_char', 'insert', str))
         character_insertion_inputs.append(Renamer.ActionInput('index', 'at position', int))
-
         character_deletion_inputs = []
         character_deletion_inputs.append(Renamer.ActionInput('number_of_char', 'number of character', int))
         character_deletion_inputs.append(Renamer.ActionInput('index', 'from position', int))
-
         custom_name_inputs = []
         custom_name_inputs.append(Renamer.ActionInput('new_name', 'new name', str))
-
         counter_inputs = []
         counter_inputs.append(Renamer.ActionInput('start_index', 'start at', int))
         counter_inputs.append(Renamer.ActionInput('increment', 'increment by', int))
         counter_inputs.append(Renamer.ActionInput('restart', 'restart', bool))
 
-        self.all_action_descriptors.append(Renamer.ActionDescriptor("Original Name", None, Renamer.OriginalName))
         self.all_action_descriptors.append(Renamer.ActionDescriptor("Find and replace", character_replacement_inputs, Renamer.CharacterReplacementAction))
         self.all_action_descriptors.append(Renamer.ActionDescriptor("Insert characters", character_insertion_inputs, Renamer.CharacterInsertionAction))
         self.all_action_descriptors.append(Renamer.ActionDescriptor("Delete characters", character_deletion_inputs, Renamer.CharacterDeletionAction))
@@ -54,24 +49,25 @@ class MainWidget(QWidget):
         self.all_action_descriptors.append(Renamer.ActionDescriptor("Counter", counter_inputs, Renamer.Counter))
 
         #Create Button and Layout
-        header = ['Original Files','test']
-        data_list = [('test',10)]
         self.prefix_number = 0
         self.suffix_number = 0
-        self.filename_index = 3
-        self.extension_index = 5
         self.suffix_boxes = []
         self.prefix_boxes = []
+        self.main_grid = QGridLayout(self)
+        self.setLayout(self.main_grid)
+        directory = "/home/pierre/Desktop/test"
+        self.model = QFileSystemModel();
+        self.model.setRootPath(directory)
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setRootIndex(self.model.index(directory))
+        self.path_box = ActionButtonGroup("Path", self.all_action_descriptors)
+        self.main_layout = QHBoxLayout()
         self.preview_btn = QPushButton('Preview')
         self.preview_btn.clicked.connect(self.apply_action)
-        self.path_box = ActionButtonGroup("Path")
-        self.path_box.set_action_descriptors(self.all_action_descriptors)
-        self.path_box.setObjectName('path_box')
-        self.filename_box = ActionButtonGroup("Filename")
-        self.filename_box.set_action_descriptors(self.all_action_descriptors)
+        self.filename_box = ActionButtonGroup("Filename", self.all_action_descriptors)
         self.filename_box.setObjectName('filename_box')
-        self.extension_box = ActionButtonGroup("Extension")
-        self.extension_box.set_action_descriptors(self.all_action_descriptors)
+        self.extension_box = ActionButtonGroup("Extension", self.all_action_descriptors)
         self.extension_box.setObjectName('extension_box')
         self.path_lbl = QLabel('Path')
         self.filename_lbl = QLabel('Filename')
@@ -79,86 +75,52 @@ class MainWidget(QWidget):
         self.add_prefix_btn = QPushButton('+')
         self.remove_prefix_btn = QPushButton('-')
         self.add_suffix_btn = QPushButton('+')
+        self.remove_suffix_btn = QPushButton('-')
         self.add_prefix_btn.clicked.connect(self.add_prefix)
         self.remove_prefix_btn.clicked.connect(self.remove_prefix)
         self.add_suffix_btn.clicked.connect(self.add_suffix)
-        self.main_grid = None
-        directory = "/home/pierre/Desktop/test"
-        self.model = QFileSystemModel();
-        self.model.setRootPath(directory)
-        self.tree = QTreeView()
-        self.tree.setModel(self.model)
-        self.tree.setRootIndex(self.model.index(directory))
-        self.add_widgets()
-        #table_model = MyTableModel(self, data_list, header)
-        #table_view = QTableView(self)
-        #table_view.setModel(table_model)
-        ## set column width to fit contents (set font first!)
-        ##table_view.resizeColumnsToContents()
-        ## enable sorting
-        ##table_view.setSortingEnabled(True)
-        #table_view.setColumnWidth(0,440)
-        #table_view.setColumnWidth(1,440)
-        #table_view.setGeometry(10, 350, 880, 300)
-        #self.action_dict = action_dict
-        #self.setGeometry(300,300,300,150)
-        #for i, file_modified in enumerate(self.files.get_files_list()['new']):
-         #   data_list.append([self.files.get_files_list()['old'][i], file_modified])
-    def add_widgets(self):
-        #add AciontButtonGroup to the grid depending on the number of prefixes and suffixes
-        self.main_grid = QGridLayout()
-        prefix_layout = QVBoxLayout()
-        prefix_layout.addWidget(self.add_prefix_btn)
-        prefix_layout.addWidget(self.remove_prefix_btn)
-        spacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        prefix_layout.addItem(spacerItem)
-        #self.main_grid.addWidget(self.preview_btn, 2, 0, 1, 1)
-        self.main_grid.addWidget(self.path_box, 0, 0, 1, 1)
-        self.main_grid.addLayout(prefix_layout, 0, 1, 1, 1)
-        if self.prefix_number != 0:
-            for i in range(len(self.prefix_boxes)):
-                #the last prefix should be added to the most left of the grid.
-                self.main_grid.addWidget(self.prefix_boxes[len(self.prefix_boxes) - i -1], 0, (i + 2), 1, 1)
-        self.main_grid.addWidget(self.filename_box, 0,(self.filename_index), 1, 1)
-        self.main_grid.addWidget(self.add_suffix_btn, 0, (self.filename_index + 1), 1, 1)
-        if self.suffix_number != 0:
-            for i, suffix_box in enumerate(self.suffix_boxes):
-                self.main_grid.addWidget(suffix_box, 0, (self.filename_index + i + 2), 1, 1)
-        self.main_grid.addWidget(self.extension_box, 0, (self.extension_index), 1, 1)
-        #self.tree.show()
-        self.main_grid.addWidget(self.tree, 1, 0, 1, 5)
-        self.setLayout(self.main_grid)
+        self.remove_suffix_btn.clicked.connect(self.remove_suffix)
+        self.prefix_layout = QVBoxLayout()
+        self.prefix_layout.addWidget(self.add_prefix_btn)
+        self.prefix_layout.addWidget(self.remove_prefix_btn)
+        self.suffix_layout = QVBoxLayout()
+        self.suffix_layout.addWidget(self.add_suffix_btn)
+        self.suffix_layout.addWidget(self.remove_suffix_btn)
+        self.main_layout.addWidget(self.path_box)
+        self.main_layout.addLayout(self.prefix_layout)
+        self.main_layout.addWidget(self.filename_box)
+        self.main_layout.addLayout(self.suffix_layout)
+        self.main_layout.addWidget(self.extension_box)
+        self.main_grid.addLayout(self.main_layout,0,0)
+        self.main_grid.addWidget(self.tree, 1, 0)
+
+    def clearLayout(self, layout):
+        """delete all children of the specified layout"""
+        while layout.count():
+            child = layout.takeAt(0)
+            if child.widget() is not None:
+                child.widget().deleteLater()
+            elif child.layout() is not None:
+                self.clearLayout(child.layout())
 
     def add_prefix(self):
         self.prefix_number += 1
-        self.filename_index += 1
-        self.extension_index += 1
-        self.main_grid.destroyed.connect(self.add_widgets)
-        self.main_grid.deleteLater()
-        prefix_box = ActionButtonGroup("Prefix " + str(self.prefix_number))
-        prefix_box.set_action_descriptors(self.all_action_descriptors)
-        prefix_box.reset_layout()
-        self.prefix_boxes.append(prefix_box)
-        self.add_widgets()
+        self.prefix_box = ActionButtonGroup("Prefix " + str(self.prefix_number), self.all_action_descriptors)
+        self.main_layout.insertWidget(2,self.prefix_box)
+        self.prefix_boxes.append(self.prefix_box)
+
+    def remove_suffix(self):
+        pass
 
     def remove_prefix(self):
         self.prefix_number -= 1
-        self.filename_index -= 1
-        self.extension_index -= 1
-        self.main_grid.deleteLater()
         del self.prefix_boxes[self.prefix_number]
-        self.add_widgets()
 
     def add_suffix(self):
         self.suffix_number += 1
-        self.extension_index += 1
-        self.main_grid.deleteLater()
-        suffix_box = ActionButtonGroup("Suffix " + str(self.suffix_number))
-        suffix_box.set_action_descriptors(self.all_action_descriptors)
-        suffix_box.reset_layout()
+        self.suffix_box = ActionButtonGroup("Suffix " + str(self.suffix_number), self.all_action_descriptors)
+        self.main_layout.insertWidget(self.prefix_number + self.suffix_number + 3, self.suffix_box)
         self.suffix_boxes.append(suffix_box)
-        self.add_widgets()
-
 
     def apply_action(self):
         self.add_widgets()
@@ -181,41 +143,35 @@ class MainWidget(QWidget):
 
 class ActionButtonGroup(QWidget):
     """Group the combobox with the textboxes containing the subactions"""
-    def __init__(self, frame_name):
+    def __init__(self, frame_name, action_descriptors):
         QWidget.__init__(self)
         self.frame_name = frame_name
-        self.all_action_descriptors = None
         self.combobox = QComboBox()
-        self.label = QLabel(frame_name)
+        self.selected_action = None 
+        self.action_descriptors = action_descriptors
+        for element in action_descriptors:
+            self.combobox.addItem(str(element))
+        self.label = QLabel(self.frame_name)
         self.combobox.currentIndexChanged[int].connect(self.on_selected_action_changed)
         self.spacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.grid = QGridLayout()
+        self.setLayout(self.grid)
         self.button_inputs_dict = {}
-        self.reset_layout()
-
-    def set_action_descriptors(self, action_descriptors):
-        self.all_action_descriptors = action_descriptors
-        for element in action_descriptors:
-            self.combobox.addItem(str(element))
-
-    def reset_layout(self):
         self.grid.addWidget(self.label, 0, 0, 1, 1)
         self.grid.addWidget(self.combobox, 1, 0, 1, 1)
-        self.button_inputs_dict = {}
         self.add_sub_button()
-        self.setLayout(self.grid)
 
     def on_selected_action_changed(self, index):
-        self.selected_action = self.all_action_descriptors[index]
-        self.reset_layout()
-        #self.add_sub_button()
+        self.selected_action = self.action_descriptors[index]
+        self.button_inputs_dict = {}
+        self.add_sub_button()
 
     def add_sub_button(self):
         sub_buttons = self.grid.itemAtPosition(2,0)
         if sub_buttons is not None:
             self.clearLayout(sub_buttons)
             sub_buttons.deleteLater()
-        if self.selected_action.action_inputs is not None:
+        if self.selected_action and self.selected_action.action_inputs is not None:
             hbox = QHBoxLayout()
             self.button_inputs_dict = {}
             for arguments in (self.selected_action.action_inputs):
@@ -237,7 +193,7 @@ class ActionButtonGroup(QWidget):
                 hbox.addLayout(vbox)
                 self.button_inputs_dict[arguments.argument_name] = ""
             self.grid.addLayout(hbox,2,0,1,1)
-        self.grid.addItem(self.spacerItem,3,0,1,5)
+        self.grid.addItem(self.spacerItem,3,0,1,1)
 
     def get_text_changed(self, value):
         self.button_inputs_dict[self.sender().objectName()] = value
@@ -267,8 +223,6 @@ class ActionButtonGroup(QWidget):
 
     def get_inputs(self):
         return self.selected_action, self.button_inputs_dict
-
-
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -306,36 +260,6 @@ class MainWindow(QMainWindow):
         """Opens a dialog to allow user to choose a directory """
         flags = QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly
         d = directory = QFileDialog.getExistingDirectory(self,"Open Directory", os.getcwd(), flags)
-
-class MyTableModel(QAbstractTableModel):
-    def __init__(self, parent, mylist, header, *args):
-        QAbstractTableModel.__init__(self, parent, *args)
-        self.mylist = mylist
-        self.header = header
-    def rowCount(self, parent):
-        return len(self.mylist)
-    def columnCount(self, parent):
-        return len(self.mylist[0])
-    def data(self, index, role):
-        if not index.isValid():
-            return None
-        elif role != Qt.DisplayRole:
-            return None
-        return self.mylist[index.row()][index.column()]
-    def headerData(self, col, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return self.header[col]
-        return None
-    def sort(self, col, order):
-        """sort table by given column number col"""
-        self.emit(SIGNAL("layoutAboutToBeChanged()"))
-        self.mylist = sorted(self.mylist,
-            key=operator.itemgetter(col))
-        if order == Qt.DescendingOrder:
-            self.mylist.reverse()
-        self.emit(SIGNAL("layoutChanged()"))
-
-
 
 if __name__ == '__main__':
     main()
