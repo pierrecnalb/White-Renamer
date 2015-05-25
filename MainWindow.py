c0485116818376170bd91e0ce967b476cb82b815
pierrecnalb <pierrecnalb@mailbox.org>
2015-05-25 19:10:53 +0200

    Update MainWindow.py
    Add scrollarea

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 #author : pierrecnalb
 #copyright pierrecnalb
-#v.1.0.6
+#v.1.0.7
 import os
 import time
 import shutil
@@ -73,17 +73,23 @@ class MainWidget(QWidget):
         self.prefix_boxes = []
         #---LAYOUT---
         self.main_grid = QGridLayout(self)
+        self.main_grid.setObjectName("main_grid")
         self.main_layout = QHBoxLayout()
+        self.main_layout.setObjectName("main_layout")
         self.prefix_layout = QVBoxLayout()
+        self.prefix_layout.setObjectName("prefix_layout")
         self.suffix_layout = QVBoxLayout()
+        self.suffix_layout.setObjectName("suffix_layout")
         self.spacer_prefix = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.spacer_suffix = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.setLayout(self.main_grid)
         self.main_grid.addLayout(self.main_layout,0,0)
         #---TREE VIEW---
         self.treeView = QTreeView()
+        self.treeView.setObjectName("treeView")
         self.treeView.setAlternatingRowColors(True)
         self.model = QStandardItemModel()
+        self.model.setObjectName("model")
         self.model.setHorizontalHeaderLabels(["Original Files","Modified Files"])
         self.treeView.setModel(self.model)
         self.main_grid.addWidget(self.treeView, 1, 0)
@@ -93,25 +99,29 @@ class MainWidget(QWidget):
         self.folder_box.changed.connect(self.apply_action)
         #---PREFIX GROUP---
         self.add_prefix_btn = QToolButton()
+        self.add_prefix_btn.setObjectName("add_prefix_btn")
         self.add_prefix_btn.setText('+')
         self.add_prefix_btn.clicked.connect(self.add_prefix)
         self.remove_prefix_btn = QToolButton()
+        self.remove_prefix_btn.setObjectName("remove_prefix_btn")
         self.remove_prefix_btn.setText('-')
         self.remove_prefix_btn.clicked.connect(self.remove_prefix)
         self.prefix_layout.addWidget(QLabel())
         self.prefix_layout.addWidget(self.add_prefix_btn)
         self.prefix_layout.addWidget(self.remove_prefix_btn)
         self.prefix_layout.addItem(self.spacer_prefix)
-        self.main_layout.addLayout(self.prefix_layout)
+        #self.main_layout.addLayout(self.prefix_layout)
         #---FILE GROUP---
         self.file_box = ActionButtonGroup("File", self.all_action_descriptors)
         self.main_layout.addWidget(self.file_box)
         self.file_box.changed.connect(self.apply_action)
         #---SUFFIX GROUP---
         self.add_suffix_btn = QToolButton()
+        self.add_suffix_btn.setObjectName("add_suffix_btn")
         self.add_suffix_btn.setText('+')
         self.add_suffix_btn.clicked.connect(self.add_suffix)
         self.remove_suffix_btn = QToolButton()
+        self.remove_suffix_btn.setObjectName("remove_suffix_btn")
         self.remove_suffix_btn.setText('-')
         self.remove_suffix_btn.clicked.connect(self.remove_suffix)
         self.suffix_layout.addWidget(QLabel()) #This empty label is used to get the buttons at the same level as the combobox
@@ -124,34 +134,50 @@ class MainWidget(QWidget):
         self.main_layout.addWidget(self.extension_box)
         self.extension_box.changed.connect(self.apply_action)
         #
-        self.preview_btn = QPushButton('Preview')
+        self.preview_btn = QPushButton()
+        self.preview_btn.setObjectName("preview_btn")
         self.preview_btn.clicked.connect(self.apply_action)
         self.main_grid.addWidget(self.preview_btn, 2, 0)
-        
-    def input_directory(self, directory):
+        self.scroll_area_content= QWidget()
+        vlayout = QVBoxLayout(self.scroll_area_content)
+        fil_box = ActionButtonGroup("File", self.all_action_descriptors)
+        fil_box.setGeometry(10,10,100,100)
+        #vlayout.addWidget(QComboBox())
+        vlayout.addLayout(self.prefix_layout)
+        vlayout.insertWidget(0,fil_box)
+        vlayout.addWidget(QComboBox())
+        scrollArea = QScrollArea()
+        scrollArea.setBackgroundRole(QPalette.Dark)
+        scrollArea.setWidget(self.scroll_area_content)
+        #fil_box.setLayout(self.scroll_area_content)
+        self.main_grid.addWidget(scrollArea,3,0)
+        self.folder_icon = QIcon("/home/pierre/Documents/Programs/White-Renamer/Icons/folder_icon.svg")
+        self.file_icon = QIcon("/home/pierre/Documents/Programs/White-Renamer/Icons/file_icon.svg")
+
+    def input_directory(self, directory, recursion, show_hidden_files):
         """Process the selected directory to create the tree and modify the files"""
         tree = self.main_grid.itemAtPosition(1,0)
         self.model.clear()
         self.model.setHorizontalHeaderLabels(["Original Files","Modified Files"])
-        self.files = Renamer.FilesCollection(directory, True)
-        self.untouched_files = Renamer.FilesCollection(directory, True)
-        self.original_tree = self.untouched_files.get_basename_tree()
+        self.files = Renamer.FilesCollection(directory, recursion, show_hidden_files)
         self.preview_data = self.files.get_files()
-        self.addItems(self.model, self.preview_data, self.preview_data)
+        self.addItems(self.model, self.preview_data)
         self.treeView.resizeColumnToContents(0)
 
 
-    def addItems(self, parent, original_elements, modified_elements):
+    def addItems(self, parent, original_elements):
         """Populate the tree with the selected directory"""
         for i in range(len(original_elements)):
-        #for text, children in elements:
-            original_files = QStandardItem(original_elements[i][0].basename)
-            modified_files = QStandardItem(modified_elements[i][0].basename)
-            parent.appendRow([original_files,modified_files])
+            if original_elements[i][0].is_folder:
+                icon = self.folder_icon
+            else:
+                icon = self.file_icon
+            original_files = QStandardItem(icon , original_elements[i][0].basename)
+            modified_files = QStandardItem(original_elements[i][0].basename)
+            parent.appendRow([original_files, modified_files])
             original_children = original_elements[i][1]
-            modified_children = modified_elements[i][1]
             if original_children:
-                self.addItems(original_files, original_children, modified_children)
+                self.addItems(original_files, original_children)
 
     def modifyItems(self, parent, modified_elements):
         """Modify the tree with the selected directory"""
@@ -185,7 +211,7 @@ class MainWidget(QWidget):
     def add_suffix(self):
         self.suffix_number += 1
         self.suffix_box = ActionButtonGroup("Suffix " + str(self.suffix_number), self.limited_action_descriptors)
-        self.main_layout.insertWidget(self.prefix_number + self.suffix_number + 3, self.suffix_box)
+        self.main_layout.insertWidget(self.prefix_number + self.suffix_number + 2, self.suffix_box)
         self.suffix_boxes.append(self.suffix_box)
         self.suffix_box.changed.connect(self.apply_action)
 
@@ -248,8 +274,18 @@ class ActionButtonGroup(QWidget):
     changed = Signal() # get changes in order to refresh the preview
     def __init__(self, frame_name, action_descriptors):
         QWidget.__init__(self)
+        self.frame = QFrame(self)
+        self.frame.setObjectName("frame")
+        #self.frame.setStyleSheet("background-color: rgb(210, 210, 210);")
+        self.frame.setFrameShape(QFrame.Box)
+        self.frame.setFrameShadow(QFrame.Plain)
+        self.frame.setLineWidth(2)
+        self.frame.setGeometry(QRect(0, 0, 150, 200))
+        self.frame_grid = QGridLayout(self.frame) #this is a hidden grid to handle the objects in the frame as if it was a grid.
+        self.frame_grid.setObjectName("frame_grid")
         self.frame_name = frame_name
         self.combobox = QComboBox()
+        self.combobox.setObjectName("combobox")
         self.action_descriptors = action_descriptors
         for element in action_descriptors:
             self.combobox.addItem(str(element))
@@ -262,7 +298,7 @@ class ActionButtonGroup(QWidget):
         self.combobox.currentIndexChanged[int].connect(self.on_selected_action_changed)
         self.spacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.grid = QGridLayout()
-        self.setLayout(self.grid)
+        self.grid.setObjectName("grid")
         self.button_inputs_dict = {}
         self.grid.addWidget(self.label, 0, 0, 1, 1)
         self.grid.addWidget(self.combobox, 1, 0, 1, 1)
@@ -286,9 +322,11 @@ class ActionButtonGroup(QWidget):
             sub_buttons.deleteLater()
         if self.selected_action and self.selected_action.action_inputs is not None:
             form = QFormLayout()
+            form.setObjectName("form")
             self.button_inputs_dict = {}
             for arguments in (self.selected_action.action_inputs):
                 label = QLabel()
+                label.setObjectName("label")
                 label.setText(str(arguments.argument_caption))
                 if arguments.argument_type == str:
                     sub_button = QLineEdit()
@@ -307,6 +345,8 @@ class ActionButtonGroup(QWidget):
                 self.button_inputs_dict[arguments.argument_name] = ""
             self.grid.addLayout(form,2,0,1,1)
         self.grid.addItem(self.spacerItem,3,0,1,1)
+        self.frame_grid.addLayout(self.grid,0,0,1,1)
+        #self.setLayout(self.frame_grid)
 
     def radio_button_clicked(self, enabled):
         if enabled:
@@ -361,16 +401,24 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
         self.setWindowTitle('Renamer')
+        self.use_subfolder = False
+        self.show_hidden_files = False
         # setGeometry(x_pos, y_pos, width, height)
-        self.setGeometry(200,200,300,400)
-        # exit option for the menu bar File menu
-        self.openAction = QAction('Open', self)
+        self.setGeometry(200,200,800,600)
+        # open option for the menu bar File menu
+        self.openAction = QAction(QIcon("/home/pierre/Documents/Programs/White-Renamer/Icons/folder_icon.svg"),'&Open', self)
         self.openAction.setShortcut('Ctrl+o')
         self.openAction.triggered.connect(self.openDirectoryDialog)
+        self.recursionAction = QAction('Modify Subfolders Recursively', self)
+        self.recursionAction.setShortcut('Ctrl+u')
+        self.recursionAction.triggered.connect(self.recursion)
+        self.hiddenFilesAction = QAction('Show Hidden Files', self)
+        self.hiddenFilesAction.setShortcut('Ctrl+h')
+        self.hiddenFilesAction.triggered.connect(self.hide_files)
+
         self.exit = QAction('Exit', self)
         # message for the status bar if mouse is over Exit
         self.exit.setStatusTip('Exit program')
-        # newer connect style (PySide/PyQT 4.5 and higher)
         #self.exit.triggered.connect(app.quit)
         # create the menu bar
         menubar = self.menuBar()
@@ -379,12 +427,66 @@ class MainWindow(QMainWindow):
         file.addAction(self.exit)
         # create the status bar
         self.statusBar()
+        self.main_toolbar = self.addToolBar('main_toolbar')
+        self.main_toolbar.addAction(self.openAction)
+        self.hide_files_btn = QCheckBox("Show Hidden Files")
+        self.hide_files_btn.setObjectName('hide_files_btn')
+        self.hide_files_btn.stateChanged[int].connect(self.hide_files)
+        
+        self.recursion_btn = QCheckBox("Show Subdirectories")
+        self.recursion_btn.setObjectName('recursion_btn')
+        self.recursion_btn.stateChanged[int].connect(self.recursion)
 
-        self.toolbar = self.addToolBar('Open')
-        self.toolbar.addAction(self.openAction)
+        #self.folder_icon = QIcon()
+        #self.folder_icon.addPixmap(QPixmap(":/icons/Icons/folder.png"), QIcon.Normal, QIcon.On)
+    #def addActions(self, target, actions):
+    #    '''Actions are added to Tool Bar.'''
+    #    for action in actions:
+    #        if action is None:
+    #            target.addSeparator()
+    #        else:
+    #            target.addAction(action)
+    #
+    #def editAction(self, action, slot=None, shortcut=None, icon=None,
+    #                 tip=None):
+    #    '''This method adds to action: icon, shortcut, ToolTip,\
+    #    StatusTip and can connect triggered action to slot '''
+    #    if icon is not None:
+    #        action.setIcon(QIcon(":/%s.png" % (icon)))
+    #    if shortcut is not None:
+    #        action.setShortcut(shortcut)
+    #    if tip is not None:
+    #        action.setToolTip(tip)
+    #        action.setStatusTip(tip)
+    #    if slot is not None:
+    #        action.triggered.connect(slot)                        
+    #    return action
+
+        self.main_toolbar.addWidget(self.hide_files_btn)
+        self.main_toolbar.addWidget(self.recursion_btn)
+        #self.recursion_toolbar = self.addToolBar('Recursion')
+        #self.recursion_toolbar.addAction(self.recursionAction)
+        #self.hidden_files_toolbar = self.addToolBar('Recursion')
+        #self.hidden_files_toolbar.addAction(self.hiddenFilesAction)
         # QWidget or its instance needed for box layout
         self.widget = MainWidget()
         self.setCentralWidget(self.widget)
+
+    @Slot()
+    def recursion(self, value):
+        if value == 0:
+            self.use_subfolder = False
+        elif value == 2:
+            self.use_subfolder = True
+        self.widget.input_directory(self.directory, self.use_subfolder, self.show_hidden_files)
+
+    @Slot()
+    def hide_files(self, value):
+        if value == 0:
+            self.show_hidden_files = False
+        elif value == 2:
+            self.show_hidden_files = True
+        self.widget.input_directory(self.directory, self.use_subfolder, self.show_hidden_files)
 
     @Slot()
     def openFileDialog(self):
@@ -397,7 +499,7 @@ class MainWindow(QMainWindow):
         """Opens a dialog to allow user to choose a directory """
         flags = QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly
         self.directory = QFileDialog.getExistingDirectory(self,"Open Directory", os.getcwd(), flags)
-        self.widget.input_directory(self.directory)
+        self.widget.input_directory(self.directory, False, False)
 
 
 if __name__ == '__main__':
