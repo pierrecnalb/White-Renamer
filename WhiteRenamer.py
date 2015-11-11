202390e39e43c8dba544e807a585782c59339e29
pierrecnalb <pierrecnalb@mailbox.org>
2015-11-11 18:03:38 +0100

    remove hidden files

--- WhiteRenamer.py
+++ WhiteRenamer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #author : pierrecnalb
 #copyright pierrecnalb
 import os
@@ -33,7 +34,7 @@ def main():
     win.show()
     try:
         code_online = urllib.request.urlopen("https://raw.githubusercontent.com/pierrecnalb/White-Renamer/master/WhiteRenamer.py").read().splitlines()
-        version_online = code_online[12].decode().split('=')[-1]
+        version_online = code_online[13].decode().split('=')[-1]
         version_online = version_online.split("'")[1]
         if(version_online != __version__):
             win.update_message(__version__, "version_online")
