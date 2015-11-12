3667780db347e99eaa8269f999fd31467d9fae99
pierrecnalb <pierrecnalb@mailbox.org>
2015-11-12 12:38:51 +0100

    hide terminal window

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-"""Fichier d'installation de notre script salut.py."""
+import sys
 from cx_Freeze import setup, Executable
-
+base = None
+icon = r'white_renamer'
+if sys.platform == "win32":
+    base = "Win32GUI"
 setup(
     name = "White Renamer",
     version = "1.0",
     description = "User friendly batch renamer",
-    executables = [Executable("WhiteRenamer.py")],
+    executables = [Executable("WhiteRenamer.py", base=base)],
 )
