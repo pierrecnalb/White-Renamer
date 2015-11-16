567e56c916b0134fe037d5b7c38953176801d95f
pierrecnalb <pierrecnalb@mailbox.org>
2015-11-16 20:09:45 +0100

    add documentation

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import sys
 from cx_Freeze import setup, Executable
 base = None
 icon = r'white_renamer'
+buildOptions = dict(include_files=["Documentation\\Documentation.pdf"])
 if sys.platform == "win32":
     base = "Win32GUI"
 setup(
     name = "White Renamer",
     version = "1.0",
     description = "User friendly batch renamer",
+    options = dict(build_exe = buildOptions),
     executables = [Executable("WhiteRenamer.py", base=base)],
 )
