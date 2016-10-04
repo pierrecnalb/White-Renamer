8eb0a4e97d5ed32ba8a28f27eb9d9fcaffc02a2e
Pierre <pierre@debian>
2016-10-04 19:30:14 +0200

    add range

---
+++ whiterenamer/model/range.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2015-2016 Pierre Blanc
+#
+# This file is part of WhiteRenamer.
+#
+# WhiteRenamer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WhiteRenamer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WhiteRenamer. If not, see <http://www.gnu.org/licenses/>.
+
+
+class Range(object):
+    def __init__(self, start, end=None):
+        """
+        """
+        self._start = start
+        self._end = end
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        self._start = value
+
+    @property
+    def end(self):
+        return self._start
+
+    @end.setter
+    def end(self, value):
+        self._end = value
